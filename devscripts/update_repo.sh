228268ecb6c3f49c4c8b7b10226c218c2d2a2ec4
YeetlePrime <jonas.tolkemitt@gmail.com>
2024-04-19 18:41:41 +0200

    Add theme for sddm

--- devscripts/update_repo.sh
+++ devscripts/update_repo.sh
@@ -1,12 +1,32 @@
 #!/bin/env sh
 homedir=$( getent passwd "$USER" | cut -d: -f6 )
 scriptdir="$(dirname $0)"
-configdir="${scriptdir}/../config"
+repodir="${scriptdir}/.."
+configdir="${repodir}/config"
 
+#create directories if they don't exist yet
+mkdir -p "${configdir}/.config"
+mkdir -p "${configdir}/sddm/themes"
+
+# zsh
 cp "${homedir}/.zshrc" "${configdir}"
 cp -r "${homedir}/.zsh" "${configdir}"
+
+# hyprland
 cp -r "${homedir}/.config/hypr" "${configdir}/.config"
+
+# waybar
 cp -r "${homedir}/.config/waybar" "${configdir}/.config"
+
+# kitty
 cp -r "${homedir}/.config/kitty" "${configdir}/.config"
+
+# qt6ct
 cp -r "${homedir}/.config/qt6ct" "${configdir}/.config"
+
+# dolphin
 cp "${homedir}/.config/dolphinrc" "${configdir}/.config"
+
+# sddm config and theme
+cp "/etc/sddm.conf.d/sddm.conf" "${configdir}/sddm"
+cp -r "/usr/share/sddm/themes/sddm-astronaut-theme" "${configdir}/sddm/themes"
