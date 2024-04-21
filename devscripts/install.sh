9288b5fbd2531974e2e5ba7ac20eb521ac0b7889
YeetlePrime <jonas.tolkemitt@gmail.com>
2024-04-21 18:48:32 +0200

    Update install script
    Now outputs a more descriptive error when not executed with root
    privileges

--- devscripts/install.sh
+++ devscripts/install.sh
@@ -1,23 +1,40 @@
 #!/bin/env sh
+
+# █░█ ▄▀█ █▀█ █▀
+# ▀▄▀ █▀█ █▀▄ ▄█
+
 homedir=$( getent passwd "$USER" | cut -d: -f6 )
 scriptdir="$(dirname $0)"
 repodir="${scriptdir}/.."
 configdir="${scriptdir}/../config"
 
-#create directories if they don't exist yet
-mkdir -p "${homedir}/.config"
-mkdir -p "/etc/sddm.conf.d"
-mkdir -p "/usr/share/sddm/themes"
-mkdir -p "/usr/local/share/fonts"
 
-# zsh
+# █▀▄ █▀▀ █▀▀ ▄▀█ █░█ █░░ ▀█▀
+# █▄▀ ██▄ █▀░ █▀█ █▄█ █▄▄ ░█░
+
+# make config directories
+mkdir -p "${homedir}/.config"
+
+# config for zsh 
 cp -f "${configdir}/.zshrc" "${homedir}"
 cp -rf "${configdir}/.zsh" "${homedir}"
 
 # copy all files from .config
 cp -rf "${configdir}/.config/." "${homedir}/.config"
 
-# sddm config and theme
-cp -f "${configdir}/sddm/sddm.conf" "/etc/sddm.conf.d/sddm.conf"
-cp -rf "${configdir}/sddm/themes/." "/usr/share/sddm/themes/"
-cp -rf "${repodir}/fonts/." "/usr/local/share/fonts/"
+
+# █▀█ █▀█ █▀█ ▀█▀
+# █▀▄ █▄█ █▄█ ░█░
+#
+if [[ "$(id -u)" -ne 0 ]] then
+	echo "The configuration for SDDM and the installation of fonts requires root privileges."
+else
+	mkdir -p "/etc/sddm.conf.d"
+	mkdir -p "/usr/share/sddm/themes"
+	mkdir -p "/usr/local/share/fonts"
+
+	# sddm config and theme
+	cp -f "${configdir}/sddm/sddm.conf" "/etc/sddm.conf.d/sddm.conf"
+	cp -rf "${configdir}/sddm/themes/." "/usr/share/sddm/themes/"
+	cp -rf "${repodir}/fonts/." "/usr/local/share/fonts/"
+fi
