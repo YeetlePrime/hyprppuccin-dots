54f0497d668c32edebd930182ed6603a60ecf466
YeetlePrime <jonas.tolkemitt@gmail.com>
2024-04-22 19:35:41 +0200

    Confifure find_files to find hidden files aswell

--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -57,11 +57,25 @@ return {
 				--  All the info you're looking for is in `:help telescope.setup()`
 				defaults = {
 					path_display = { "smart" },
+					vimgrep_arguments = {
+						"rg",
+						"--hidden",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+					},
 					mappings = {
 						i = { ["<c-enter>"] = "to_fuzzy_refine" },
 					},
 				},
-				-- pickers = {}
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
