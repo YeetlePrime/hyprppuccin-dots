6a80cafdc6763d74c3e3b2dde49eaa45fb066319
YeetlePrime <jonas.tolkemitt@gmail.com>
2024-06-06 17:22:46 +0200

    Add oil to nvim plugins and refactor import strategy

--- config/.config/nvim/lua/plugins/oil.lua
+++ config/.config/nvim/lua/plugins/oil.lua
@@ -5,4 +5,8 @@ return {
 			show_hidden = true,
 		},
 	},
+	config = function(opts)
+		vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "Open Oil Explorer" })
+		require("oil").setup(opts)
+	end,
 }
