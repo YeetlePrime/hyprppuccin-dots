36ab8fbbf801a6efbcfa4ab2e8a9f013fa139075
YeetlePrime <jonas.tolkemitt@gmail.com>
2024-06-06 17:54:03 +0200

    Change default shell to zsh in install script

--- devscripts/full_install.sh
+++ devscripts/full_install.sh
@@ -6,4 +6,11 @@ sudo "$scriptdir/install_pacman_dependencies.sh"
 "$scriptdir/install_aur_dependencies.sh"
 sudo "$scriptdir/apply_config.sh"
 
+# Change default shell to zsh, if not already
+temp="$(grep ^"$USER" /etc/passwd)"
+current_shell="${temp: -3}"
+if [[ "${current_shell}" != "zsh" ]]; then
+	chsh -s "$(which zsh)"
+fi
+
 kbuildsycoca6 # rebuild system configuration
